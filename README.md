f1c1efa2edd26054411e4a0342c658e30a2eebaa
Primo Data <mershon.max16@gmail.com>
2023-06-13 04:01:35 +0000

    proof of concept

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# Dune Analytics x Pandas AI
+# Dune AI Analyst
 
-<p align="left">
-<a href="https://github.com/gventuri/pandas-ai/releases"><img alt="Package version" src="https://img.shields.io/badge/PandasAI_Version-0.5.0-376eff.svg"></a>
-<a href="https://github.com/psf/black"><img alt="Code style: black" src="https://img.shields.io/badge/Code%20style-black-000000.svg"></a>
-</p>
+[![GitHub Codespaces Ready](https://img.shields.io/badge/GitHub%20Codespaces-Ready-green)](https://github.com/features/codespaces)
+![OpenAI Badge](https://img.shields.io/badge/Powered%20by-OpenAI-1B6AC6.svg)
+[![Code style: black](https://img.shields.io/badge/Code%20style-black-000000.svg)](https://github.com/psf/black)
+
+A simple [Streamlit](https://streamlit.io/) app that retrieves data from any [Dune Analytics](https://dune.com/) query using its API and allows users to ask questions about the data using [Pandas AI](https://github.com/gventuri/pandas-ai).
--- app.py
+++ app.py
@@ -5,10 +5,11 @@ from pandasai.llm.openai import OpenAI
 import pandas as pd
 import requests
 import json
+import re
 
 # Configure Streamlit Page
 page_icon = "https://dune.com/assets/DuneLogoCircle.svg"
-st.set_page_config(page_title="DuneAI", page_icon=page_icon, layout="wide")
+st.set_page_config(page_title="Dune AI Analyst", page_icon=page_icon, layout="wide")
 
 # Read Custom CSS
 with open("assets/css/style.css", "r") as f:
@@ -18,7 +19,7 @@ st.markdown(custom_css, unsafe_allow_html=True)
 
 # OpenAI "powered by" component
 powered_by = """
-<div style="display: flex; align-items: center;">
+<div style="display: flex; align-items: center; margin-bottom:20px">
     <span style="font-size: 14px; margin-right: 4px; font-style: italic;">Powered by:</span>
     <img src="https://www.freelogovectors.net/wp-content/uploads/2023/01/openai-logo-freelogovectors.net_.png" alt="OpenAI logo" height="24">
 </div>
@@ -28,62 +29,57 @@ powered_by = """
 openai_api_key = os.environ["OPENAI_KEY"]
 dune_api_key = os.environ["DUNE_KEY"]
 
-# Setup Pandas AI
-llm = OpenAI(openai_api_key)
-pandas_ai = PandasAI(llm, save_charts=True)
+# Heading
+st.write(
+    f'<h1><img src="https://dune.com/assets/DuneLogoCircle.svg" alt="OpenAI logo" height="36" style="margin-bottom:6px">  Dune AI Analyst</h1>',
+    unsafe_allow_html=True,
+)
+st.subheader("Analyze any Dune dataset using natural language")
+st.markdown(powered_by, unsafe_allow_html=True)
 
 
 # Query Dune Analytics using API
 def query_dune(q):
-    url = f"https://api.dune.com/api/v1/query/2296642/results?api_key={dune_api_key}"
+    url = f"https://api.dune.com/api/v1/query/{q}/results?api_key={dune_api_key}"
     response = requests.get(url)
     results_json = json.loads(response.text)["result"]["rows"]
     results_df = pd.DataFrame.from_dict(results_json)
     return results_df
 
 
-# Load data
-@st.cache_data(show_spinner="Pulling Dune data...")
-def load_data():
-    # https://dune.com/queries/2296642
-    q = "2296642"
-    df = query_dune(q)
-    return df
+# Initialize Session State
+if "df" not in st.session_state:
+    st.session_state["df"] = ""
 
+col1, col2 = st.columns(2)
 
-# Fetch data
-df = load_data()
+# Query Dune
+with col1:
+    query = st.text_input("(1) Enter a Dune Query Id.")
+    submit_query = st.button("Query Data")
+    if submit_query:
+        st.session_state.df = query_dune(query)
+        st.table(st.session_state.df.head())
+    elif isinstance(st.session_state.df, pd.DataFrame):
+        st.table(st.session_state.df.head())
 
-# Sidebar
-st.image(
-    "https://cdn.sanity.io/images/22xmfoma/production/e8ff6d4fe83614b4b7ed6ea9e864cf526c4c0e3c-2134x293.png",
-    width=150,
-)
-st.markdown(powered_by, unsafe_allow_html=True)
-st.header("Dune AI")
-st.info("This is an experimental AI tool to analyze data using natural language.")
+# Ask Question
+with col2:
+    # Setup Pandas AI
+    llm = OpenAI(openai_api_key)
+    pandas_ai = PandasAI(llm, save_charts=True)
 
-# Body
-queryid = st.text_input("Enter a Dune query id:", help="")
-submit_queryid = st.button("Pull Query Data")
-st.table(df.head())
+    # Analyze Data
+    question = st.text_input("(2) Ask a question or plot results.")
+    submit_question = st.button("Analyze Data")
 
-question = st.text_input("Ask me a question or tell me to plot something.", help="")
-submit_question = st.button("Submit")
-
-# Display the text when the button is clicked
-"""
-if submit_question:
-    st.write("Question:", question)
-    st.write("Thinking...")
-    answer = pandas_ai(df, prompt=question)
-
-    try:
-        # Display image if generated
-        pattern = r"Charts saved to: (.*)"
-        file_path = re.search(pattern, answer).group(1)
-        st.image(f"{file_path}/chart.png")
-    except:
-        # Otherwise display text
-        st.write("Answer:", answer)
-"""
+    if submit_question:
+        answer = pandas_ai(st.session_state.df, prompt=question)
+        try:
+            # Display image if generated
+            pattern = r"Charts saved to: (.*)"
+            file_path = re.search(pattern, answer).group(1)
+            st.image(f"{file_path}/chart.png")
+        except:
+            # Otherwise display text
+            st.write("Answer:", answer)
